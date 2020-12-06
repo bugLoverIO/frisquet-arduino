afca7aaadfc9753ecd1a3d9c6b1006950602a43c
Buglover <74265687+bugLoverIO@users.noreply.github.com>
2020-12-06 18:16:03 +0100

    Ajoute le support du stuffing sous flag
    Par défaut la gestion du stuffing est activé, commenter le flag STUFFING_MNG

--- frisquet-ERS-command/frisquet-ERS-command.ino
+++ frisquet-ERS-command/frisquet-ERS-command.ino
@@ -1,84 +1,113 @@
-byte ERS_pin = 3;
-int long_pulse = 825;
-byte message[17] = {0x00, 0x00, 0x00, 0x7E, 0xBE, 0xE7, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD, 0x00, 0xFF, 0x80};
-byte old_state, num_byte;
-byte bitstuff = 0;
+// A commenter pour avoir la trame brute. 
+#define STUFFING_MNG
 
-void writeBit(bool Bit) {
-  old_state = !old_state;
-  digitalWrite(ERS_pin, old_state);
-  delayMicroseconds(long_pulse);
-
-  if (Bit) {
-    old_state = !old_state;
-    digitalWrite(ERS_pin, old_state);
-  }
-  delayMicroseconds(long_pulse);
-
-}
-
-void conversion(byte input) {
-  for (byte n = 0; n < 8; n++) { // boucle pour chaque bit
-    writeBit(bitRead(input, n));
-    if (num_byte >= 4 && num_byte <= 14) {
-      if (bitRead(input, n) == 1) bitstuff++;  // incrémente le compteur bitstuffing
-    }
-    if (bitRead(input, n) == 0) bitstuff = 0;
-
-    if (bitstuff >= 5) {
-      writeBit(0);
-      bitstuff = 0;
-    }
-  }
-}
-
-void commande(byte prechauffage, byte chauffage) {
-  Serial.print("Nouvelle commande de chauffage : (");
-  Serial.print(prechauffage);
-  Serial.print(",");
-  Serial.print(chauffage);
-  Serial.println(")");
-  Serial.flush();
-
-  if ((chauffage <= 100) and ((prechauffage == 0) or (prechauffage == 3) or (prechauffage == 4))) {
-    for (byte x = 0; x < 3; x++) { // boucle de 3 messages
-      old_state = 0;
-      message[9] = x;  // numero message : 0 à 2
-      if (x == 2) {
-        message[10] = prechauffage;
-      } else {
-        message[10] = prechauffage + 0x80;
-      }
-      message[11] = chauffage;
-
-      int checksum = 0;
-      for (int i = 4; i <= 12; i++) {
-        checksum -= message[i];
-      }
-      message[13] = highByte(checksum);
-      message[14] = lowByte(checksum);
-
-      for (num_byte = 1; num_byte < 17; num_byte++) { // boucle de 16 bytes
-        conversion(message[num_byte]);
-      }
-      //digitalWrite(ERS_pin, HIGH);
-      digitalWrite(ERS_pin, LOW);
-      delay(33);
-    }
-    digitalWrite(ERS_pin, LOW);
-
-  } else {
-    return;
-  }
-}
-
-
+volatile long duration = 0;
+volatile long prev_time = 0;
+volatile boolean data_dispo = false;
+byte input = 3;
+String trame[3];
+byte message = 0;
+byte start = true;
+byte intro = 0;
+String prefixe = "1100";
+ 
 void setup() {
-  pinMode(ERS_pin, OUTPUT);
-  digitalWrite(ERS_pin, LOW);
+  Serial.begin(500000);
+  attachInterrupt(digitalPinToInterrupt(input), declenche, CHANGE);
+}
+ 
+void loop() {
+  if (data_dispo == true) {
+    int bit_state = digitalRead(input);
+    if ((duration > 700) && (duration < 950)) { // pulse off court
+      trame[message] = trame[message] + !bit_state;
+    } else if ((duration > 1400) && (duration < 1950)) { // pulse off long
+      // verifie qu'on commence avec un pulse long à 1
+      if ((start == true) && (!bit_state == HIGH)) {
+        trame[message] = trame[message] + !bit_state + !bit_state;
+        start = false;
+      } else if (start == false) {
+        trame[message] = trame[message] + !bit_state + !bit_state;
+      }
+    } else {
+      start = true; // debut d'une nouvelle trame
+      if (trame[message].length() > 200) {
+        message++;
+        if (message > 2) {
+          for (message = 0; message < 3; message++) {
+            for (int x = 0; x <= 50; x=x+4) {
+              if (trame[message].substring(x,x+4).compareTo(prefixe) == 0) {
+                intro++;
+              } else {
+                break;
+              }
+            }
+            String sortie = "";
+            
+            if (intro > 6 ) {
+
+              byte nbOne = 0;
+              
+              // décodage des bits de la trame: 00=0, 11=0, 10=1, 01=1
+              for (int x = (intro*4); x <= trame[message].length(); x=x+2) {
+                if ((trame[message].substring(x,x+2) == "00") or (trame[message].substring(x,x+2) == "11")) {
+#ifdef STUFFING_MNG                  
+                  if (nbOne != 5) sortie = sortie + "0";    // gestion du Stuffing pour tous les octets de la trame
+#else
+                  sortie = sortie + "0";
+#endif                                                                          
+                  nbOne = 0;
+                } else {
+                  sortie = sortie + "1";
+                  nbOne ++;
+                }
+              }
+
+              // repartition en bytes
+              String id1 = sortie.substring(0,8);
+              String id2 = sortie.substring(8,16);
+              String id3 = sortie.substring(16,24);
+              byte num = convert(sortie.substring(48,56));
+              byte prechauff = convert(sortie.substring(56,64));
+              byte chauffage = convert(sortie.substring(64,72));
+              Serial.print("num: ");
+              Serial.println(num);
+              Serial.print("prechauff: ");
+              Serial.println(prechauff);
+              Serial.print("chauffage: ");
+              Serial.println(chauffage);
+              intro = 0;
+            }
+          }  
+          trame[0] = "";
+          trame[1] = "";
+          trame[2] = "";
+          message = 0;
+        }
+      } else {
+        // Re-initialise la trame s'il n'y avait pas de données juste avant      
+        if ((message == 0) || (trame[message-1].length() < 200)) { 
+          trame[0] = "";
+          trame[1] = "";
+          trame[2] = "";
+          message = 0;
+        }
+      }
+    }
+    data_dispo = false;
+  }
 }
 
-void loop() {
-  commande(0,0);   // reduit 0, confort 3, hors gel 4, chauffage 0 à 100
-  delay(240000);  // boucle toutes les 4 minutes
+void declenche() {
+  duration = micros()-prev_time;
+  prev_time = micros();
+  data_dispo = true;
+}
+
+byte convert(String entree) {
+  byte result = 0;
+  for (int f = 0; f <= 7 ; f++) {
+    if (entree.substring(f,f+1) == "1") bitSet(result, f);
+  }
+return result;
 }
